84fcbc66c1ec3cdcf2ba6c3b18046d100211fa91
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-12-19 19:28:34 +0545

    name display in profile

--- Dashboard/Student_Dashboard/Courses Management.php
+++ Dashboard/Student_Dashboard/Courses Management.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html data-bs-theme="light" lang="en">
 
@@ -88,11 +89,13 @@
                             <li class="nav-item dropdown no-arrow">
                                 <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link"
                                         aria-expanded="false" data-bs-toggle="dropdown" href="#"><span
-                                            class="d-none d-lg-inline me-2 text-gray-600 small">xyz</span><img
+                                            class="d-none d-lg-inline me-2 text-gray-600 small">
+                                             <?php echo $_SESSION['name'];?>
+                                        </span><img
                                             class="border rounded-circle img-profile"
                                             src="assets/img/untitled-1.png"></a>
                                     <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a
-                                            class="dropdown-item" href="#"><i
+                                            class="dropdown-item" href="profile.php"><i
                                                 class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a>
                                         <div class="dropdown-divider"></div><a class="dropdown-item" href="../../front/logout.php"><i
                                                 class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a>
