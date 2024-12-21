8d52d158136b782bab9ce70d6234fa3d4ed27427
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-12-21 10:48:08 +0545

    modified routine display code

--- Dashboard/Student_Dashboard/assets/js/Timetable/routine.js
+++ Dashboard/Student_Dashboard/assets/js/Timetable/routine.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    // On semester change
     $('#semester').change(function () {
         const semester = $(this).val();
 
@@ -11,15 +10,17 @@ $(document).ready(function () {
                 data: { semester: semester },
                 success: function (response) {
                     console.log('Response Data:', response); 
-                    console.log('Course Name:', response.course_name) 
+                    console.log('Course Name:', response.course_name); 
                     if (response.success) {
                         let routineHTML = '';
                         response.data.forEach(item => {
+                            const examTime12Hour = convertTo12HourFormat(item.exam_time);
+
                             routineHTML += `
                                 <tr>
                                     <td>${item.exam_name}</td>
                                     <td>${item.exam_date}</td>
-                                    <td>${item.exam_time}</td>
+                                    <td>${examTime12Hour}</td>
                                     <td>${item.subject_name}</td>
                                     <td>${item.duration} mins</td>
                                     <td>${item.location}</td>
@@ -30,7 +31,6 @@ $(document).ready(function () {
                         $('#exam-routine').html(`<tr><td colspan="6">${response.message}</td></tr>`);
                     }
                 },
-                
                 error: function () {
                     $('#exam-routine').html('<tr><td colspan="6">Error fetching routine.</td></tr>');
                 }
@@ -40,3 +40,20 @@ $(document).ready(function () {
         }
     });
 });
+
+function convertTo12HourFormat(time) {
+    let [hours, minutes] = time.split(':');
+    let period = 'AM';
+
+    hours = parseInt(hours);
+    
+    if (hours >= 12) {
+        period = 'PM';
+        if (hours > 12) hours -= 12;
+    } else if (hours === 0) {
+        hours = 12;
+    }
+
+    minutes = minutes ? minutes : '00';
+    return `${hours}:${minutes} ${period}`;
+}
